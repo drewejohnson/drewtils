02699aadf44a8b5bee23df35872dbe364d31174b
Andrew Johnson <drewej@protonmail.com>
2020-01-04 13:41:35 -0500

    Update copyright year to include 2020

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'drewtils'
-copyright = '2017-2019, Andrew Johnson'
+copyright = '2017-2020, Andrew Johnson'
 author = 'Andrew Johnson'
 
 # The version info for the project you're documenting, acts as replacement for
